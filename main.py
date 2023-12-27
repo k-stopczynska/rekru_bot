dce84f5675cb6a78cadd0e61ebaac89ed0fbb16d
k.stopczynska <k.stopczynska@gmail.com>
2023-12-27 20:10:37 +0100

    added login func

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import time
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 """
@@ -15,7 +18,8 @@ version above 115: https://googlechromelabs.github.io/chrome-for-testing/
 And here is the list of downloads for 115 and older: 
 https://chromedriver.chromium.org/downloads
 Remember to put it in PATH executable or add it to you environmental paths
-I you don't know the version of your Chrome, type in you browser: chrome://version
+I you don't know the version of your Chrome, 
+type in you browser: chrome://version
 """
 
 cd_path = r"C:\chromedriver-win64\chromedriver.exe"
@@ -23,11 +27,20 @@ service = Service(cd_path)
 driver = wb.Chrome(service=service)
 
 
+def login():
+    driver.get("https://www.linkedin.com/home")
+    time.sleep(5)
+    driver.find_element(By.ID, "session_key").send_keys(
+        os.getenv('LINKEDIN_LOGIN'))
+    driver.find_element(By.ID, "session_password").send_keys(
+        os.getenv('LINKEDIN_PASSWORD'))
+    time.sleep(5)
+    driver.find_element(By.CSS_SELECTOR, "button").click()
+
+
 def run():
-    print("hello bot")
+    login()
 
 
 if __name__ == '__main__':
     run()
-
-
