d530cc6e616712ab33db50aa77bc5fbd157156cb
k.stopczynska <k.stopczynska@gmail.com>
2024-01-15 20:11:59 +0100

    added test for initializing webdriver

--- tests.py
+++ tests.py
@@ -1,1 +1,14 @@
-from unittest import
+from unittest import TestCase
+
+import selenium.webdriver.chrome.webdriver
+
+from web_scraping import *
+
+
+class TestRecruitmentBot(TestCase):
+
+	def test_initialize_driver(self):
+		cd_path = r"C:\chromedriver-win64\chromedriver.exe"
+		driver = initialize_driver(cd_path)
+		self.assertNotEqual(driver, None)
+		self.assertIsInstance(driver, selenium.webdriver.chrome.webdriver.WebDriver)
