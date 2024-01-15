f2dbb840239577ab0ca7eb946b13c917928ae6d3
k.stopczynska <k.stopczynska@gmail.com>
2024-01-15 18:56:43 +0100

    added write to csv function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from time import sleep
+import csv
 
 
 """
@@ -100,8 +101,8 @@ def pass_query_params(params):
 def change_page():
 
     """
-    change_page function is calling scroll down function for lazy loaded results,
-    and changes page pages_num times if "next button" is found
+    change_page function is calling scroll down function for lazy loaded
+    results, and changes page pages_num times if "next button" is found
     :excepts: any Exception raised if page couldn't be scrolled further or next
     button is not found
     """
@@ -109,7 +110,7 @@ def change_page():
     pages_num = 1
 
     for page in range(pages_num):
-        scroll_num = 10
+        scroll_num = 8
 
         for scroll in range(scroll_num):
             try:
@@ -132,8 +133,8 @@ def change_page():
 def scrape_google_results():
 
     """
-    scrape_google_results function finds links to candidates' profiles aligned
-    with search params, and returns them as a List
+    scrape_google_results function finds links to candidates' profiles and
+    their names aligned with search params
     :return: List(str) users' links list
     """
 
@@ -146,12 +147,40 @@ def scrape_google_results():
     linkedin_users_names = driver.find_elements(By.XPATH,
         '//div[@class="MjjYud"]/div/div/div/div/div/span/a/h3')
 
-    parsed_names = [[name.text.split(' ')[0], name.text.split(' ')[1]] for name in linkedin_users_names]
+    parsed_names = [[name.text.split(' ')[0], name.text.split(' ')[1]] for name
+                    in linkedin_users_names]
 
     users_data = zip(parsed_names, parsed_urls)
-    print(users_data)
+    return [data for data in users_data]
 
-    # return [[user.text.split(' ')[1], user.text.split(' ')[1], user.get_attribute('href')] for user in users_data]
+
+def save_to_csv(data, csv_filename='users.csv'):
+
+    """
+    save_to_csv function writes data to a csv file for future use
+    :param data: data List to write into csv file
+    :param csv_filename: filename of a file that is used to save results
+    """
+
+    with open(csv_filename, mode='a', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ['lp', 'first name', 'last name', 'linkedin url']
+        csv_writer = csv.DictWriter(csv_file, fieldnames)
+        lp = 0
+
+        if csv_file.tell() == 0:
+            csv_writer.writeheader()
+
+        for entry in data:
+
+            lp += 1
+
+            (first_name, last_name), linkedin_url = entry
+
+            csv_writer.writerow(
+                {'lp': lp, 'first name': first_name, 'last name': last_name,
+                 'linkedin url': linkedin_url})
+
+    print(f'Data has been written to {csv_filename}')
 
 
 def search_in_google(params):
@@ -173,7 +202,9 @@ def search_in_google(params):
 
     change_page()
 
-    return scrape_google_results()
+    users = scrape_google_results()
+
+    save_to_csv(users, csv_filename='users.csv')
 
 
 def run():
@@ -184,7 +215,7 @@ def run():
 
     skillset, location, role = define_search_parameters()
     google_params = skillset + [role, location]
-    links = search_in_google(google_params)
+    search_in_google(google_params)
 
 
 if __name__ == '__main__':
